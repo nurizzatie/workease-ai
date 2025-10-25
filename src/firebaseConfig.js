2aa7d3e5c6e391b3905636a04665d15400fc3ee1
nurizzatie <nurizzati.jsl@gmail.com>
2025-10-25 22:20:53 +0800

    Change setup firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDNYnzHUPdVToLbQEKjM59yC6Ab9XQmCVU",
-  authDomain: "workease-ai.firebaseapp.com",
-  projectId: "workease-ai",
-  storageBucket: "workease-ai.firebasestorage.app",
-  messagingSenderId: "522610867469",
-  appId: "1:522610867469:web:bc1ecf9d9dbbc528558444"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
